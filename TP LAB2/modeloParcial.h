50a75b8b67805e0d47acffafd666cbc9aa96a151
caeli1010 <70002068+caeli1010@users.noreply.github.com>
2020-09-24 19:30:02 -0300

    m

--- TP LAB2/modeloParcial.h
+++
@@ -1,7 +0,0 @@
-#ifndef MODELOPARCIAL_H_INCLUDED
-#define MODELOPARCIAL_H_INCLUDED
-
-void tiempoPromedio(); /// Punto A
-void entrenamientos2020(); /// Punto B
-
-#endif // MODELOPARCIAL_H_INCLUDED
